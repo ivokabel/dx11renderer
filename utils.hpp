78c5fc5c301538f8cbbc56a42e87362f6f579586
Ivo Pavlík <ivokabel@users.noreply.github.com>
2019-02-05 16:49:56 +0100

    minor

--- utils.hpp
+++ utils.hpp
@@ -25,10 +25,10 @@ namespace Utils
         if (level < sLoggingLevel)
             return;
 
-        wchar_t tmpBuff1[1024] = { 0 };
+        wchar_t tmpBuff1[1024] = {};
         swprintf_s(tmpBuff1, msg, args...);
 
-        wchar_t tmpBuff2[1024] = { 0 };
+        wchar_t tmpBuff2[1024] = {};
         swprintf_s(tmpBuff2, L"[% 7s] %s\n", LogLevelToString(level), tmpBuff1);
 
         OutputDebugString(tmpBuff2);
